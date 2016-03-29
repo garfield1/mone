573192394bdb6c5766b88412ba9b13f8f6c5e8f3
xufengtian14@163.com <xufengtian@localhost.localdomain>
2016-03-29 16:49:37 +0800

    发布申请页面测试页面

--- src/views/release_apply.py
+++ src/views/release_apply.py
@@ -1,20 +1,40 @@
 #!/usr/bin/env python
 #encoding=utf-8
-from flask import Blueprint
-from _release_apply import *
+from ConfigParser import ConfigParser
+# import json
+# import os
+# from django.db.models import Q
+from flask import Blueprint, render_template, request, session, redirect, url_for
+from flask.ext.login import login_required
+import time
+# from models.mone.models import
+# from views._release_apply import state_transfer
+config = ConfigParser()
+with open('mone.conf', 'r') as cfgfile:
+	config.readfp(cfgfile)
+	page_size = int(config.get('page', 'page_size'))
+	upload_path = config.get('path', 'upload_path')
+cfgfile.close()
+
+
 release_apply = Blueprint('release_apply', __name__)
 
-@release_apply.route('/')
-def index():
-	ll = len(User.objects.all())
-	print ll
-	return str(ll)
+@release_apply.route('/taskpad/', methods=['GET'])
+@login_required
+def taskpad():
+	return render_template("release_apply/taskpad.html")
 
-if __name__ == "__main__":
-	from _django_orm import *
-	from flask import Flask
-	app = Flask('my_application')
-	app.register_blueprint(release_apply, url_prefix='/release_apply')
-	print User.objects.all()
-	print "zz"
-	app.run(host='0.0.0.0',debug= True)
+@release_apply.route('/add/', methods=['GET'])
+@login_required
+def add():
+	return render_template("release_apply/add.html")
+
+@release_apply.route('/list/', methods=['GET'])
+@login_required
+def list():
+	return render_template("release_apply/list.html")
+
+@release_apply.route('/detail/', methods=['GET'])
+@login_required
+def detail():
+	return render_template("release_apply/details.html")
