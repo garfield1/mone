c192b8fc8ec43a030765258983164186fed3a813
xufengtian14 <xufengtian14@163.com>
2016-04-05 11:07:58 +0800

    xxx

--- src/static/js/release_apply/details.js
+++ src/static/js/release_apply/details.js
@@ -4,7 +4,7 @@
 $("#worksheet_content").html($("#worksheet_content").attr("content"));
 
 var action_type = '';
-var worksheet_id = $('#worksheet_id').text();
+var release_apply_id = $('#release_apply_id').val();
 var reject_reason = '';
 
 $(".decision").click(function(){
@@ -15,25 +15,37 @@ $(".decision").click(function(){
         $("#tips").hide();
         reject_advice.find("label").text("请填写打回原因(必填):");
         reject_advice.show();
+        $("#toSave").addClass("disabled");
         action_type = $("#last_action").val();
     } else {
         $("#tips").hide();
-        reject_advice.find("label").text("请填写通过意见:");
+        reject_advice.find("label").text("请填写通过意见(选填):");
         reject_advice.show();
-        action_type = "主管确认";
+        $("#toSave").removeClass("disabled");
+        action_type = $("#next_action").val();
+    }
+});
+
+$("#reject-reason").keyup(function(){
+    var this_elt = $(this);
+    if ( $.trim(this_elt.val()) != "" ){
+        $("#toSave").removeClass("disabled");
+    }else{
+        $("#toSave").addClass("disabled");
     }
 });
 
 $("#toSave").click(function(){
     reject_reason = $("#reject-reason").val();
-    $.post("/worksheet/update/worksheetstate/", {
+    $.post("/release_apply/update/releaseapplystate/", {
         action_type: action_type,
-        worksheet_id: worksheet_id,
+        release_apply_id: release_apply_id,
         reject_reason: reject_reason
     }, function(result){
         if (result.status == "200") {
             location.reload();
         }else{
+            alert("数据库异常！请联系运维开发人员！")
         }
     }, "json");
 });
