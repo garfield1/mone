56421a85be1ea535e6cace79b50459a63bf876e8
huangfuzepeng <111111>
2016-04-08 10:14:51 +0800

    添加构建进程

---
+++ src/crons/mone_build_queue.py
@@ -0,0 +1,59 @@
+# coding=utf-8
+import re
+import gevent
+import time
+import subprocess
+from _django_orm import *
+
+def get_file_name(git_url):
+    '''
+	获取文件名
+	:param git_url:
+	:return:
+	'''
+    file_path = git_url.split('/')[-1]
+    p = re.compile(r'([\s\S]*).git')
+    file_name = p.findall(file_path)
+    return file_name[0]
+
+
+def save_build_message(release_apply_id, message, created_at):
+    applicationbuild_data = ReleaseapplyBuild(release_apply_id=release_apply_id, message=message, created_at=created_at)
+    applicationbuild_data.save()
+
+def build(git_url, release_apply_id):
+    '''
+	构建
+	:param git_url:
+	:return:
+	'''
+    file_name = get_file_name(git_url)
+    command_rm = 'rm -rf {0}'.format(file_name)
+    command_git = 'git clone {0}'.format(git_url)
+    if os.path.exists(file_name):
+        os.system(command_rm)
+    os.system(command_git)
+    os.chdir(file_name)
+    command_bulit = 'mvn clean package'
+    ps = subprocess.Popen(command_bulit, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+    while True:
+        data = ps.stdout.readline()
+        if ps.poll() is not None:
+            break
+        else:
+            ISOTIMEFORMAT = '%Y-%m-%d %X'
+            now_time = time.strftime(ISOTIMEFORMAT, time.localtime(time.time()))
+            save_build_message(release_apply_id, data, now_time)
+
+def main():
+    for build_queue in BulidQueue.objects.filter(is_build=False):
+        releaseapplybuilds = ReleaseapplyBuild.objects.filter(release_apply_id=build_queue.release_apply_id)
+        releaseapplybuilds.delete()
+        g1 = gevent.spawn(build, build_queue.git_url, build_queue.release_apply_id)
+        g1.join()
+        build_queue.is_build = True
+        build_queue.save()
+
+
+if __name__ == "__main__":
+    main()
