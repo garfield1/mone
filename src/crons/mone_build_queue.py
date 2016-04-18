9c863bcd456c7953538c2f028ce5c205e4a0ee74
huangfuzepeng <111111>
2016-04-18 18:03:01 +0800

    update

--- src/crons/mone_build_queue.py
+++ src/crons/mone_build_queue.py
@@ -88,8 +88,9 @@ def main():
         except Exception, e:
             formal_mvn_command = 'mvn clean package'
         save_mvn_file(build_queue.release_apply.application_id, build_queue.release_apply.application.file_path, build_queue.release_apply.application.name)
-        g1 = gevent.spawn(build, build_queue.git_url, build_queue.release_apply_id, formal_mvn_command)
-        g1.join()
+        # g1 = gevent.spawn(build, build_queue.git_url, build_queue.release_apply_id, formal_mvn_command)
+        # g1.join()
+        build(build_queue.git_url, build_queue.release_apply_id, formal_mvn_command)
         ISOTIMEFORMAT = '%Y-%m-%d %X'
         updated_at = time.strftime(ISOTIMEFORMAT, time.localtime(time.time()))
         release_apply_data = build_queue.release_apply
